d46d11293465ea0f63d45dd0fea3bc76a5cfceb8
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-01 10:46:40 +0800

    homePage sections added

--- src/page-controllers/homePage.jsx
+++ src/page-controllers/homePage.jsx
@@ -5,7 +5,22 @@ class homePage extends React.Component {
   render() {
     return (
       <div className="snow">
-        <h1>Homepage</h1>
+        <section className="homePage-hero">
+          <h1 className="homePage-hero-title">Homepage</h1>
+        </section>
+        <section className="homePage-map">
+          <ul>
+            <li>
+              <button>city 1</button>
+            </li>
+            <li>
+              <button>city 2</button>
+            </li>
+            <li>
+              <button>city 3</button>
+            </li>
+          </ul>
+        </section>
       </div>
     );
   }

305ac5f1144eef599f5ddde04638d6fa4f330929
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-03 17:01:24 +0800

    Button as class component and used successfully in map

--- src/components/DestinationButton.jsx
+++ src/components/DestinationButton.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import "../App.css";
 
 class DestinationButton extends React.Component {
+  //destinationId = "5f4f0c4d6dc6a63a00dfd95a";
+  displayInfo = () => {
+    fetch(
+      "https://omt3u.sse.codesandbox.io/api/destinations/" +
+        this.props.destinationId
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   render() {
-    return <button>{this.props.destination}</button>;
+    return <button onClick={this.displayInfo}>{this.props.destination}</button>;
   }
 }
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,23 +3,19 @@ import "../App.css";
 import DestinationButton from "./DestinationButton.jsx";
 
 class Map extends React.Component {
-  cityBio = () => {
-    let id = "5f4f0c4d6dc6a63a00dfd95a";
-    fetch("https://omt3u.sse.codesandbox.io/api/destinations/" + id)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  };
-
   render() {
     return (
       <div>
         <section className="homePage-map">
           <ul>
             <li>
-              <button onClick={this.cityBio}>city 1</button>
+              <button>city 1</button>
             </li>
             <li>
-              <DestinationButton destination="city 2" />
+              <DestinationButton
+                destination="Hakuba Valley"
+                destinationId="5f4f0c4d6dc6a63a00dfd95a"
+              />
             </li>
             <li>
               <button>city 3</button>

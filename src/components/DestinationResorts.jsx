636e24c2e4e35442b1492b63ec3239c92556c471
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-10 13:43:36 +0800

    DestinationResorts converted to Class with function child component

--- src/components/DestinationResorts.jsx
+++ src/components/DestinationResorts.jsx
@@ -1,30 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "../App.css";
 
-function DestinationResorts({ match }, props) {
-  useEffect(() => {
-    getResorts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const [resorts, setResorts] = useState({});
-
-  const getResorts = async () => {
+class DestinationResorts extends React.Component {
+  getResorts = async (props) => {
     try {
       const url = new URL("https://5sx1m.sse.codesandbox.io/api/resorts");
-      const params = { destinationId: match.params.id };
+      const params = { destinationId: this.props.match.params.id };
       url.search = new URLSearchParams(params).toString();
       const response = await fetch(url);
       const resorts = await response.json();
-      setResorts(resorts);
-      console.log(resorts);
+      //console.log(resorts);
+      this.setState({ results: resorts });
+      console.log(this.state.results);
     } catch (e) {
       console.log(e);
     }
   };
+  render() {
+    return (
+      <div className="destination-resorts">
+        <h1 className="page-title">Resorts</h1>
+        <ResortsChild displayResorts={this.getResorts} />
+      </div>
+    );
+  }
+}
+
+function ResortsChild(props) {
+  const { displayResorts } = props;
+  useEffect(() => {
+    displayResorts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="destination-resorts">
-      <h1 className="page-title">Resorts</h1>
+    <div>
+      <h6>results</h6>
     </div>
   );
 }

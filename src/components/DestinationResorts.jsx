3d8ab26af21c233357e9502c5d799cc9c1d4687b
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-10 13:20:35 +0800

    deleted ResortsChild

--- src/components/DestinationResorts.jsx
+++ src/components/DestinationResorts.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
-import ResortsChild from "./ResortsChild.jsx";
 
 function DestinationResorts({ match }, props) {
   useEffect(() => {
-    //console.log(match.params.id);
     getResorts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -12,15 +10,14 @@ function DestinationResorts({ match }, props) {
   const [resorts, setResorts] = useState({});
 
   const getResorts = async () => {
-    //console.log();
     try {
       const url = new URL("https://5sx1m.sse.codesandbox.io/api/resorts");
       const params = { destinationId: match.params.id };
       url.search = new URLSearchParams(params).toString();
       const response = await fetch(url);
       const resorts = await response.json();
-      //console.log(resorts);
       setResorts(resorts);
+      console.log(resorts);
     } catch (e) {
       console.log(e);
     }
@@ -28,7 +25,6 @@ function DestinationResorts({ match }, props) {
   return (
     <div className="destination-resorts">
       <h1 className="page-title">Resorts</h1>
-      <ResortsChild array={resorts} />
     </div>
   );
 }

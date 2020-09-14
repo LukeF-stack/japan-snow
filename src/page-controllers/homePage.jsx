d5cf2132d3035c66820fc2d843be85e58a2023bb
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-14 15:51:14 +0800

    UserContext base created

--- src/page-controllers/homePage.jsx
+++ src/page-controllers/homePage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../App.css";
 import HomePageHero from "../components/HomePageHero.jsx";
 import Map from "../components/Map.jsx";
+import { UserContext } from "../components/UserContext";
 
 function HomePage() {
+  const msg = useContext(UserContext);
   return (
     <div className="home">
       <HomePageHero />
       <h1>MAP</h1>
+      <p>{msg}</p>
       <Map />
     </div>
   );

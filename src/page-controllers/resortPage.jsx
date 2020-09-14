e181bec35eec757f2331d7e8ccfa99113870820f
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-14 13:30:54 +0800

    added resortPage description

--- src/page-controllers/resortPage.jsx
+++ src/page-controllers/resortPage.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
-class ResortPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="page-title">This is a resort</h1>
-      </div>
-    );
-  }
+function ResortPage({ match }) {
+  useEffect(() => {
+    generatePageContent();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const [resort, setResort] = useState({});
+  const generatePageContent = async () => {
+    try {
+      const response = await fetch(
+        `https://5sx1m.sse.codesandbox.io/api/resorts/${match.params.id}`
+      );
+      const resort = await response.json();
+      setResort(resort);
+      console.log(resort);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <h1 className="page-title">{resort.title}</h1>
+      <p>{resort.description}</p>
+    </div>
+  );
 }
 
 export default ResortPage;

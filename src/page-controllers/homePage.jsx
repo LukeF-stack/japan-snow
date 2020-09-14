3f1f10a2903e9cd1a6f313948e1e4bb015039472
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-14 15:55:31 +0800

    userContext msg removed from homepage

--- src/page-controllers/homePage.jsx
+++ src/page-controllers/homePage.jsx
@@ -1,16 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 import "../App.css";
 import HomePageHero from "../components/HomePageHero.jsx";
 import Map from "../components/Map.jsx";
-import { UserContext } from "../components/UserContext";
+//import { UserContext } from "../components/UserContext";
 
 function HomePage() {
-  const msg = useContext(UserContext);
+  //const msg = useContext(UserContext);
   return (
     <div className="home">
       <HomePageHero />
       <h1>MAP</h1>
-      <p>{msg}</p>
       <Map />
     </div>
   );

bf4faaee7cce36fe8c150063cd428e14ef2d3d9a
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-19 16:02:15 +0800

    user taken to homepage after login

--- src/components/SignInUser.jsx
+++ src/components/SignInUser.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useContext } from "react";
 import "../App.css";
 import { UserContext } from "./UserContext";
+import { useHistory } from "react-router-dom";
 
-function SetUser(props) {
+function SignInUser(props) {
   const { userData } = props;
   userData["authenticated"] = true;
   const { setUser } = useContext(UserContext);
+  const history = useHistory();
   useEffect(() => {
     setUser(userData);
-  }, [setUser, userData]);
+    history.push("/");
+  }, [setUser, userData, history]);
   return null;
 }
 
-export default SetUser;
+export default SignInUser;

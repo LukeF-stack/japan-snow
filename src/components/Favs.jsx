e0fc4cb683245a66439c7cbc163770604e3d2063
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-22 14:07:42 +0800

    favs destinations successfully fetched on refresh

--- src/components/Favs.jsx
+++ src/components/Favs.jsx
@@ -13,37 +13,45 @@ function Favs() {
   useEffect(() => {
     getFavDestinations();
     //console.log(user.favs_destinations);
-  }, [setState]);
+  }, [user]);
 
   const getFavDestinations = async () => {
-    try {
-      const url = new URL("https://5sx1m.sse.codesandbox.io/api/destinations");
-      const params = { ids: user.favs_destinations };
-      url.search = new URLSearchParams(params).toString();
-      const response = await fetch(url);
-      const destinations = await response.json();
-      //console.log(resorts);
-      //const results = resorts;
-      console.log(destinations);
-      destinations.forEach((destination) => {
-        //console.log(destination);
-        list.push(
-          <li key={destination._id}>
-            <Link to={`/destinations/${destination._id}/info`}>
-              <div className="resort-result">
-                <h5>{destination.title}</h5>
-                <img src={destination.cover_img} alt={destination.title}></img>
-              </div>
-            </Link>
-          </li>
+    if (user.favs_destinations) {
+      try {
+        const url = new URL(
+          "https://5sx1m.sse.codesandbox.io/api/destinations"
         );
+        const favsIds = await user.favs_destinations;
+        const params = await { ids: favsIds };
+        url.search = new URLSearchParams(params).toString();
+        const response = await fetch(url);
+        const destinations = await response.json();
+        //console.log(resorts);
+        //const results = resorts;
+        console.log(destinations);
+        destinations.forEach((destination) => {
+          //console.log(destination);
+          list.push(
+            <li key={destination._id}>
+              <Link to={`/destinations/${destination._id}/info`}>
+                <div className="resort-result">
+                  <h5>{destination.title}</h5>
+                  <img
+                    src={destination.cover_img}
+                    alt={destination.title}
+                  ></img>
+                </div>
+              </Link>
+            </li>
+          );
 
-        setState(list);
-        //console.log("list is ", list);
-        //this.setState({ list: this.list });
-      });
-    } catch (e) {
-      console.log(e.message);
+          setState(list);
+          //console.log("list is ", list);
+          //this.setState({ list: this.list });
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
     }
   };
   return (

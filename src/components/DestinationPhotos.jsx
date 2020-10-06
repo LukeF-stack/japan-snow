7366bb6f1005c33126141017cdb4f4b1d57134b5
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-10-06 10:21:26 +0800

    gallery starter deleted destination id passed through to PhotoGallery component successfully

--- src/components/DestinationPhotos.jsx
+++ src/components/DestinationPhotos.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../App.css";
+import PhotoGallery from "./PhotoGallery";
+//import PhotoGallery from "./PhotoGallery";
 //import PhotoGallery from "./PhotoGallery";
 
 function DestinationPhotos(props) {
   return (
     <div className="destination-photos">
       <h1 className="section-title">Photos</h1>
+      <PhotoGallery id={props.match.params.id} />
     </div>
   );
 }
